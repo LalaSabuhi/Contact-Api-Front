2e00eea77969dcfe9770bf88d84ded49901b27f5
LalaSabuhi <yavuzadpu@gmail.com>
2024-05-15 15:55:27 +0400

    add footer

--- src/Api/ContactService.jsx
+++ src/Api/ContactService.jsx
@@ -1,22 +1,27 @@
 import axios from "axios";
 
-const API_URL ='http://localhost:8080/contacts';
+const API_URL = 'http://localhost:8080/contacts';
 
-export async function saveContact(contact){
-  return await async.post(API_URL, contact);
+export async function saveContact(contact) {
+    return await axios.post(API_URL, contact);
 }
-export async function getContacts(page=0, size=10){
-  return await async.get(`${API_URL}?page=${page}&size=${size}`);
+
+export async function getContacts(page = 0, size = 10) {
+    return await axios.get(`${API_URL}?page=${page}&size=${size}`);
 }
-export async function getContact(id){
-  return await async.post(`${API_URL}/${id}`);
+
+export async function getContact(id) {
+    return await axios.get(`${API_URL}/${id}`);
 }
-export async function updateContact(contact){
-  return await async.post(API_URL, contact);
+
+export async function udpateContact(contact) {
+    return await axios.post(API_URL, contact);
 }
-export async function updatePhoto(formData){
-  return await async.put(`${API_URL}/photo`, formData);
+
+export async function udpatePhoto(formData) {
+    return await axios.put(`${API_URL}/photo`, formData);
 }
-export async function deleteContact(id){
-  return await async.delete(`${API_URL}/${id}`);
+
+export async function deleteContact(id) {
+    return await axios.delete(`${API_URL}/${id}`);
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ const Header = ({ toggleModal, nbOfContacts }) => {
     <header className='header'>
       <div className='container'>
         <h3>
-          Contact List {nbOfContacts}
+          Contact List ({nbOfContacts})
         </h3>
         <button className='btn' onClick={() => toggleModal(true)}><i className='bi bi-plus-quare'></i> Add New Contact </button>
       </div>
